731ad9d6a90b960cff090d8bbb3223cdca924137
keli214 <77420536+keli214@users.noreply.github.com>
2024-03-06 21:29:12 -0800

    subarray sum equals k

---
+++ lc560.cpp
@@ -0,0 +1,24 @@
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int subarraySum(vector<int> nums, int target) {
+    unordered_map<int, int> m {{0,-1}}; //key: sum, val: idx
+    int curr = 0, res = 0;
+    for(int i = 0; i < nums.size(); i ++){
+        curr += nums[i]; 
+        if(m.find(curr-target) != m.end()){
+            res = max(res, i-m[curr-target]);
+        }
+        if(m.find(curr) == m.end()){
+            m[curr] = i;
+        }
+    }
+    
+    return res;
+}
+int main(){
+    vector<int> nums {1,2,1,2,1};
+    cout<< subarraySum(nums, 3);
+}
