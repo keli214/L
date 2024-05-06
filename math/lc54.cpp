591d132a0f873abcde05c81e66acf5cb40058d98
keli214 <77420536+keli214@users.noreply.github.com>
2024-05-05 22:44:13 -0700

    Spiral Matrix

--- math/lc54.cpp
+++ math/lc54.cpp
@@ -5,26 +5,24 @@ using namespace std;
 
 int dir[5] = {0,1,0,-1,0};
 vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    int r = 0, c = 0, count = 0, n = matrix.size(), m = matrix[0].size(), d = 0, l = 0, currC = m, currR = n-1, curr = currC;
+    int r = 0, c = 0, count = 0, n = matrix.size(), m = matrix[0].size();
+    int d = 0, l = 0, currC = m, currR = n-1, curr = currC;//starting in the first row
     vector<int> res;
     while(count < n*m){
-        // cout<<r<<c<<endl;
         res.push_back(matrix[r][c]);
         count ++;
         l++;
         //need to change dir
         if(l == curr){
-            //row 
-            if(d%2==0){
+            if(d%2==0){//going in a row  
                 currC --;
                 curr = currR;
-            }else{
+            }else{//going in a col
                 currR --;
                 curr = currC;
             }
             d = (d + 1)%4;
             l = 0;
-            // cout<<curr<<endl;
         }
         r += dir[d];
         c += dir[d + 1];
